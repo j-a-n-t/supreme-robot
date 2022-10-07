dbbd369392851e78fa7b881a44227f96113ed209
Alexis Torres <j.a.n.t.200296@outlook.com>
2022-10-06 21:33:49 -0500

    fix acomonando importaciones

--- nativeApp/App.tsx
+++ nativeApp/App.tsx
@@ -1,21 +1,22 @@
-import React from "react";
-import HolaMundoScreen from "./src/screens/HolaMundoScreen";
-import BoxObjectModelScreen from "./src/screens/BoxObjectModelScreen";
-import { SafeAreaView } from "react-native";
-import DimensionesScreen from "./src/screens/DimensionesScreen";
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+
+// import HolaMundoScreen from './src/screens/HolaMundoScreen';
+// import BoxObjectModelScreen from './src/screens/BoxObjectModelScreen';
 // import HolaMundoScreen from "./src/screens/HolaMundoScreen";
 // import CounterScreen from "./src/screens/CounterScreen";
+import DimensionesScreen from './src/screens/DimensionesScreen';
 
 const App = () => {
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-      {/* <HolaMundoScreen />*/}
-      {/* <CounterScreen />*/}
-      {/*<BoxObjectModelScreen />*/}
-      <DimensionesScreen />
-    </SafeAreaView>
+    return (
+        <SafeAreaView style={{flex: 1}}>
+            {/* <HolaMundoScreen />*/}
+            {/* <CounterScreen />*/}
+            {/*<BoxObjectModelScreen />*/}
+            <DimensionesScreen/>
+        </SafeAreaView>
 
-  );
+    );
 };
 
 export default App;
