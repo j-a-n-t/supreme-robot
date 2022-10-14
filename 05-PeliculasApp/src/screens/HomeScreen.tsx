75e628b4a2f48121298a1d43f9d5a15b6c1d3906
alexis torres <j.a.n.t.200296@outlook.com>
2022-10-14 11:48:21 -0500

    reconf: paquetes para el carrousel funcione

--- 05-PeliculasApp/src/screens/HomeScreen.tsx
+++ 05-PeliculasApp/src/screens/HomeScreen.tsx
@@ -1,37 +1,38 @@
-import { Button, Text, View } from "react-native";
+import {Dimensions, View} from "react-native";
 import Carousel from "react-native-snap-carousel";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { NavigationProp, useNavigation } from "@react-navigation/native";
+import {useSafeAreaInsets} from "react-native-safe-area-context";
+// import { NavigationProp, useNavigation } from "@react-navigation/native";
 
-import { useMovies } from "../hooks/useMovies";
-import { IsLoading } from "../components/IsLoading";
-import MoviePoster from "../components/MoviePoster";
+import {useMovies} from "../hooks/useMovies";
+import {IsLoading} from "../components/IsLoading";
+import {MoviePoster} from "../components/MoviePoster";
+
+
+const {width: windoWidth} = Dimensions.get("window");
 
 const HomeScreen = () => {
-  const navigation = useNavigation<NavigationProp<any>>();
-  const { newMovies, isLoading } = useMovies();
-  const { top } = useSafeAreaInsets();
+    // const navigation = useNavigation<NavigationProp<any>>();
+    const {newMovies, isLoading} = useMovies();
+    const {top} = useSafeAreaInsets();
 
-  if (isLoading) {
-    return <IsLoading />;
-  }
+    if (isLoading) {
+        return <IsLoading/>;
+    }
 
 
-  return (
-    <View style={{ marginTop: top + 20 }}>
-      {
-        !newMovies
-          ? <Text>Sin datos</Text>
-          : <>
-            {/*<MoviePoster movie={newMovies[0]} />*/}
+    return (
+        <View style={{marginTop: top + 20}}>
+
             <Carousel
-              data={newMovies}
-              renderItem={() => <MoviePoster movie={newMovies[0]} />} />
-          </>
+                layout={"default"}
+                data={newMovies ? newMovies : []}
+                renderItem={({item}) => <MoviePoster movie={item}/>}
+                sliderWidth={windoWidth}
+                itemWidth={300}
+            />
 
-      }
 
-    </View>);
+        </View>);
 };
 
-export { HomeScreen };
+export {HomeScreen};
