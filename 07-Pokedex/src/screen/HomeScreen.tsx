82b76c5d61af5a7ab56ab65e34521532d894fa77
Alexis Torres <j.a.n.t.200296@outlook.com>
2022-10-25 17:30:24 -0500

    feat: colocando iconos a Pokedex

--- 07-Pokedex/src/screen/HomeScreen.tsx
+++ 07-Pokedex/src/screen/HomeScreen.tsx
@@ -1,5 +1,6 @@
 import { Text, View } from "react-native";
 import { useContext } from "react";
+import Icon from "react-native-vector-icons/Ionicons";
 
 import { ThemeContext } from "../context/themeContext";
 
@@ -8,6 +9,7 @@ const HomeScreen = () => {
   return (
     <View>
       <Text style={{ color: colors.text }}>Home Screen</Text>
+      <Icon name="albums" size={30} color="#900" />
     </View>
   );
 };
